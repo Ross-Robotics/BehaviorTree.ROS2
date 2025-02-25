b3adc4372f7a59607e7171ca08f4bb8c367dff85
Eduard Rius <151645546+edu-rossrobotics@users.noreply.github.com>
2025-02-25 13:54:48 +0000

    Revert "Update include"

--- behaviortree_ros2/src/tree_execution_server.cpp
+++ behaviortree_ros2/src/tree_execution_server.cpp
@@ -19,7 +19,7 @@
 #include "behaviortree_cpp/loggers/groot2_publisher.h"
 
 // generated file
-#include <behaviortree_ros2/bt_executor_parameters.hpp>
+#include "bt_executor_parameters.hpp"
 namespace
 {
 static const auto kLogger = rclcpp::get_logger("bt_action_server");
